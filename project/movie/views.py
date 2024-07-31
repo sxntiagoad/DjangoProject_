af252f22b999df20554c46353e30a97a23973b13
sxntiagoad <salvarezd3@eafit.edu.co>
2024-07-31 11:23:25 -0500

    nuevo archivo about.html

--- project/movie/views.py
+++ project/movie/views.py
@@ -6,4 +6,4 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page :D</h1>')
     return render(request, 'home.html', {'name':'Santiago Alvarez'})
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>') 
+    return render(request,'about.html')
